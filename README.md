50eeefde565f5f65e55cc17edc6f01c016d7be04
Sergi <sergisergi2001@gmail.com>
2023-02-02 15:12:14 +0100

    Ultims canvis

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 # chargetrip_app
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
-- Test Sergi
+- Per tal que l'aplicació funcioni correctament, aconsellem executar-la en un dispoitiu 
+amb Android 13 (Tiramisu), que es la versió que hem utilitzat per a testrjat l'app.
+- 
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
-import 'dart:async';
-
 import 'package:chargetrip_app/bottom_navigation/navigator_pages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:chargetrip_app/routes/locations.dart';
+
 import 'package:chargetrip_app/bottom_navigation/navigator.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 import 'authentication/login_screen.dart';
 
 
 void main() {
-  //Hacemos que la app solo se pueda ejecutar en vertical para evitar problemas
+  // Hacemos que la app solo se pueda ejecutar en vertical para evitar problemas
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((value) => runApp(const MyApp()));
 }
